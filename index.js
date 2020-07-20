f328730364ca8076069a27caa3b2542157ee488d
Alessandro Amarri <amarri.alessandro@gmail.com>
2020-07-20 17:39:42 +0100

    Added files

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length = this.items.length;
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length) {
+      throw new Error('OutOfBounds');
+    } else {
+      const index = this.items.indexOf(pos);
+      return this.items[index];
+    }
+  }
 
-  max() {}
+  max() {
+    if (!this.length) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.max(...this.items);
+    }
+  }
 
-  min() {}
+  min() {
+    if (!this.length) {
+      throw new Error('EmptySortedList');
+    } else {
+      return Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    let totalSum = 0;
+    for (const element of this.items) {
+      console.log(element);
+      totalSum += element;
+      //console.log(totalSum);
+    }
+  }
 
   avg() {}
 }
